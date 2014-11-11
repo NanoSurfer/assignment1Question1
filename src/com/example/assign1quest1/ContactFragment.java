471432e85df736ee3965fa4cc5482efc63f1fe5f
Shawn Brewis <shawnbrewis@gmail.com>
2014-11-11 23:40:19 +0200

    First Commit

---
+++ src/com/example/assign1quest1/ContactFragment.java
@@ -0,0 +1,283 @@
+package com.example.assign1quest1;
+
+import android.animation.Animator;
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ContactFragment extends Fragment {
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+
+	@Override
+	public void setArguments(Bundle args) {
+		// TODO Auto-generated method stub
+		super.setArguments(args);
+	}
+
+	@Override
+	public void setInitialSavedState(SavedState state) {
+		// TODO Auto-generated method stub
+		super.setInitialSavedState(state);
+	}
+
+	@Override
+	public void setTargetFragment(Fragment fragment, int requestCode) {
+		// TODO Auto-generated method stub
+		super.setTargetFragment(fragment, requestCode);
+	}
+
+	@Override
+	public void onHiddenChanged(boolean hidden) {
+		// TODO Auto-generated method stub
+		super.onHiddenChanged(hidden);
+	}
+
+	@Override
+	public void setRetainInstance(boolean retain) {
+		// TODO Auto-generated method stub
+		super.setRetainInstance(retain);
+	}
+
+	@Override
+	public void setHasOptionsMenu(boolean hasMenu) {
+		// TODO Auto-generated method stub
+		super.setHasOptionsMenu(hasMenu);
+	}
+
+	@Override
+	public void setMenuVisibility(boolean menuVisible) {
+		// TODO Auto-generated method stub
+		super.setMenuVisibility(menuVisible);
+	}
+
+	@Override
+	public void setUserVisibleHint(boolean isVisibleToUser) {
+		// TODO Auto-generated method stub
+		super.setUserVisibleHint(isVisibleToUser);
+	}
+
+	@Override
+	public void startActivity(Intent intent) {
+		// TODO Auto-generated method stub
+		super.startActivity(intent);
+	}
+
+	@Override
+	public void startActivity(Intent intent, Bundle options) {
+		// TODO Auto-generated method stub
+		super.startActivity(intent, options);
+	}
+
+	@Override
+	public void startActivityForResult(Intent intent, int requestCode) {
+		// TODO Auto-generated method stub
+		super.startActivityForResult(intent, requestCode);
+	}
+
+	@Override
+	public void startActivityForResult(Intent intent, int requestCode,
+			Bundle options) {
+		// TODO Auto-generated method stub
+		super.startActivityForResult(intent, requestCode, options);
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// TODO Auto-generated method stub
+		super.onActivityResult(requestCode, resultCode, data);
+	}
+
+	@Override
+	public void onInflate(AttributeSet attrs, Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onInflate(attrs, savedInstanceState);
+	}
+
+	@Override
+	public void onInflate(Activity activity, AttributeSet attrs,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onInflate(activity, attrs, savedInstanceState);
+	}
+
+	@Override
+	public void onAttach(Activity activity) {
+		// TODO Auto-generated method stub
+		super.onAttach(activity);
+	}
+
+	@Override
+	public Animator onCreateAnimator(int transit, boolean enter, int nextAnim) {
+		// TODO Auto-generated method stub
+		return super.onCreateAnimator(transit, enter, nextAnim);
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+
+	@Override
+	public void onViewCreated(View view, Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onViewCreated(view, savedInstanceState);
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onActivityCreated(savedInstanceState);
+	}
+
+	@Override
+	public void onViewStateRestored(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onViewStateRestored(savedInstanceState);
+	}
+
+	@Override
+	public void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+	}
+
+	@Override
+	public void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		// TODO Auto-generated method stub
+		super.onSaveInstanceState(outState);
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		// TODO Auto-generated method stub
+		super.onConfigurationChanged(newConfig);
+	}
+
+	@Override
+	public void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+
+	@Override
+	public void onStop() {
+		// TODO Auto-generated method stub
+		super.onStop();
+	}
+
+	@Override
+	public void onLowMemory() {
+		// TODO Auto-generated method stub
+		super.onLowMemory();
+	}
+
+	@Override
+	public void onTrimMemory(int level) {
+		// TODO Auto-generated method stub
+		super.onTrimMemory(level);
+	}
+
+	@Override
+	public void onDestroyView() {
+		// TODO Auto-generated method stub
+		super.onDestroyView();
+	}
+
+	@Override
+	public void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+
+	@Override
+	public void onDetach() {
+		// TODO Auto-generated method stub
+		super.onDetach();
+	}
+
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		// TODO Auto-generated method stub
+		super.onCreateOptionsMenu(menu, inflater);
+	}
+
+	@Override
+	public void onPrepareOptionsMenu(Menu menu) {
+		// TODO Auto-generated method stub
+		super.onPrepareOptionsMenu(menu);
+	}
+
+	@Override
+	public void onDestroyOptionsMenu() {
+		// TODO Auto-generated method stub
+		super.onDestroyOptionsMenu();
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onOptionsMenuClosed(Menu menu) {
+		// TODO Auto-generated method stub
+		super.onOptionsMenuClosed(menu);
+	}
+
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+		// TODO Auto-generated method stub
+		super.onCreateContextMenu(menu, v, menuInfo);
+	}
+
+	@Override
+	public void registerForContextMenu(View view) {
+		// TODO Auto-generated method stub
+		super.registerForContextMenu(view);
+	}
+
+	@Override
+	public void unregisterForContextMenu(View view) {
+		// TODO Auto-generated method stub
+		super.unregisterForContextMenu(view);
+	}
+
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		return super.onContextItemSelected(item);
+	}
+	
+}
